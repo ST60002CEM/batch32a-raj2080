7805fd640f9ebc122be902fbc6d4524418a16de3
raj2080 <decemsmith2080@gmail.com>
2024-05-18 21:06:01 +0545

    welcome/splash screen has been made

--- final_assignment/lib/app/app.dart
+++ final_assignment/lib/app/app.dart
@@ -1,22 +1,20 @@
-import 'package:flutter/material.dart';
 import 'package:login_signup/screens/welcome_screen.dart';
 import 'package:login_signup/theme/theme.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+import 'package:flutter/material.dart';
 
-  // This widget is the root of your application.
+class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // themes
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: lightMode,
-      home: const WelcomeScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
---
+++ final_assignment/lib/screens/welcome_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:login_signup/screens/signin_screen.dart';
+import 'package:login_signup/screens/signup_screen.dart';
+import 'package:login_signup/theme/theme.dart';
+import 'package:login_signup/widgets/custom_scaffold.dart';
+import 'package:login_signup/widgets/welcome_button.dart';
+import 'dart:async';
+
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SignInScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  return CustomScaffold(
+    child: Column(
+      children: [
+        Flexible(
+            flex: 8,
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 0,
+                horizontal: 40.0,
+              ),
+              child: Center(
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                          text: 'Welcome Back!\n',
+                          style: TextStyle(
+                            fontSize: 45.0,
+                            fontWeight: FontWeight.w600,
+                          )),
+                      TextSpan(
+                          text:
+                          '\n Shop freely',
+                          style: TextStyle(
+                            fontSize: 20,
+                            // height: 0,
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            )),
+        Flexible(
+          flex: 1,
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Row(
+              children: [
+                const Expanded(
+                  child: WelcomeButton(
+                    buttonText: 'Sign in',
+                    onTap: SignInScreen(),
+                    color: Colors.transparent,
+                    textColor: Colors.white,
+                  ),
+                ),
+                Expanded(
+                  child: WelcomeButton(
+                    buttonText: 'Sign up',
+                    onTap: const SignUpScreen(),
+                    color: Colors.white,
+                    textColor: lightColorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+}
